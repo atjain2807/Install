e9848c99f7c0c0e1f6d7f5b36d42908afde9c899
atjain2807 <47474075+atjain2807@users.noreply.github.com>
2021-04-04 14:13:00 +0530

    updated jenkins.sh file

--- jenkins/setup-Jenkins.sh
+++ jenkins/setup-Jenkins.sh
@@ -14,8 +14,11 @@ sudo apt-get install -y openjdk-8-jdk
 
 #Install Jenkins
 
-wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
-echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+##wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+##echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+    /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install -y jenkins
 sudo systemctl start jenkins
